291810ab601a87efc7abb729e57f81304085c299
tayyab212 <tthunjra@gmail.com>
2020-01-29 13:16:04 +0500

    authentication

--- src/app/common.service.ts
+++ src/app/common.service.ts
@@ -2,11 +2,14 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { IUser } from './Interface/user';
 import { Observable, Subscribable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class CommonService {
+  _URL = "http://localhost:50038"
   _urlgetAllUsers = 'http://localhost:5000/api/Common/get_alluser';
   _urlLogin = 'http://localhost:5000/api/Common/login';
 
@@ -18,7 +21,48 @@ export class CommonService {
     return this._http.get<IUser[]>(this._urlgetAllUsers);
   }
 
-  login(user): Subscribable<boolean> {
-   return this._http.post<boolean>(this._urlLogin,user);
+  login(user) {
+
+    return this._http.post<any>(`${this._URL}/api/Common/login`, user).pipe(map(user => {
+      debugger;
+      console.log(user);
+      if (user.isValid == 1) {
+        localStorage.setItem('currentUser', JSON.stringify(user));
+        return user;
+      } else {
+        return user;
+      }
+
+    }));
+
   }
+
+
+
+
+  //   login(Email: string, password: string) {
+  //     debugger;
+  //     return this.http.post<any>(`${this._URL}/api/Common/login`, { Email, password })
+  //         .pipe(map(user => {
+  //             debugger;
+  //             console.log(user);
+  //             // store user details and jwt token in local storage to keep user logged in between page refreshes
+  //             localStorage.setItem('currentUser', JSON.stringify(user));
+
+  //             this.currentUserSubject.next(user);
+
+  //             return user;
+  //         }));
+  // }
+
+  logout() {
+    // remove user from local storage to log user out
+    localStorage.removeItem('currentUser');
+    //this.currentUserSubject.next(null);
+  }
+
+
+
+
+
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonService } from '../common.service';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-
+import { first } from 'rxjs/operators';
+import { Route } from '@angular/compiler/src/core';
+import { Router } from '@angular/router';
+import { map } from 'rxjs/operators';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -11,11 +14,17 @@ export class LoginComponent implements OnInit {
   Valid: boolean;
   login: FormGroup;
 
-  constructor(private _commonService: CommonService, private fb: FormBuilder) {
+  constructor(private _commonService: CommonService, private fb: FormBuilder ,private router :Router) {
     this.login = fb.group({
       email: ['', [Validators.required, Validators.email]],
       password: ["", [Validators.required]]
     })
+
+    if(typeof(localStorage.getItem("currentUser")) =='undefined'){
+   this.router.navigate(['/']);
+  };
+
+  
 
   }
   ngOnInit() {
@@ -24,8 +33,20 @@ export class LoginComponent implements OnInit {
 
   loginUser() {
     debugger;
-    this._commonService.login(this.login.value)
-      .subscribe(data => this.Valid = data);
+    this._commonService.login(this.login.value).pipe(first()).subscribe(
+      data =>  { 
+        debugger;
+        console.log(data);
+      if(data.isValid ==1){
+        this.router.navigate(['examples'])
+      }else{
+        this.Valid =false;
+      }
+      },
+      error => {
+       alert("An error occured");
+      }
+    );
   }
 
   alertClass(){
